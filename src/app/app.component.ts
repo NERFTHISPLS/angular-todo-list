62ea53b9939b282099801bec7e3ee7a6f9e4b553
NERFTHIS <mustaevs@gmail.com>
2024-03-20 13:24:28 +0300

    Move list item layout in task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-tasks-list',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks-list.component.html',
-  styleUrl: './tasks-list.component.scss'
+  styleUrl: './tasks-list.component.scss',
 })
-export class TasksListComponent {
-
-}
+export class TasksListComponent {}
