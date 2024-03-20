1b36f55f9dbdb02f802fe619e361ba83cc501a3b
NERFTHIS <mustaevs@gmail.com>
2024-03-20 17:40:32 +0300

    Add select-task-type in tasks-search

--- src/app/tasks-search/tasks-search.component.ts
+++ src/app/tasks-search/tasks-search.component.ts
@@ -1,16 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { SelectTaskTypeComponent } from '../select-task-type/select-task-type.component';
+import { TaskTypes } from '../interfaces/task';
 
 @Component({
   selector: 'app-tasks-search',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, SelectTaskTypeComponent],
   templateUrl: './tasks-search.component.html',
   styleUrl: './tasks-search.component.scss',
 })
 export class TasksSearchComponent {
   searchForm = new FormGroup({
     searchQuery: new FormControl(''),
+    taskType: new FormControl<TaskTypes>(TaskTypes.Regular),
   });
 
   @Output() searchQueryEvent = new EventEmitter<string>();
