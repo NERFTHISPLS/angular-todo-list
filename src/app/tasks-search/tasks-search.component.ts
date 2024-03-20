742a12cfbc9a6cd7c7e088fd73b688712b76d869
NERFTHIS <mustaevs@gmail.com>
2024-03-20 15:28:42 +0300

    Create tasks-search component

---
+++ src/app/tasks-search/tasks-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks-search.component.html',
+  styleUrl: './tasks-search.component.scss'
+})
+export class TasksSearchComponent {
+
+}
