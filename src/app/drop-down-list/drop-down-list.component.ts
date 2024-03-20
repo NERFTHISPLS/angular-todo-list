dd59fc682347e3f4e4e9b7144569a5170ec753d0
NERFTHIS <mustaevs@gmail.com>
2024-03-20 20:01:37 +0300

    Create drop-down-list component

---
+++ src/app/drop-down-list/drop-down-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './drop-down-list.component.html',
+  styleUrl: './drop-down-list.component.scss'
+})
+export class DropDownListComponent {
+
+}
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../interfaces/task';
+import { DropDownListComponent } from '../drop-down-list/drop-down-list.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DropDownListComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
