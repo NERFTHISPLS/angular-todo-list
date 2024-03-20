ea78f530e8500e5a24db8bfce2ff76195d70fc6e
NERFTHIS <mustaevs@gmail.com>
2024-03-20 13:59:43 +0300

    Initialize tasks array

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Task, TaskType } from '../interfaces/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
+  tasks: Task[] = [
+    { id: 'task1', taskName: 'Some task 1', taskType: TaskType.Regular },
+    { id: 'task2', taskName: 'Some task 2', taskType: TaskType.Regular },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
