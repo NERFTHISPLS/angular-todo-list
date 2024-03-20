991e1f53e902a6ea9173d464d5dfc406928061b7
NERFTHIS <mustaevs@gmail.com>
2024-03-20 16:40:23 +0300

    Create select-task-type-component

---
+++ src/app/select-task-type/select-task-type.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-task-type',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-task-type.component.html',
+  styleUrl: './select-task-type.component.scss'
+})
+export class SelectTaskTypeComponent {
+
+}
