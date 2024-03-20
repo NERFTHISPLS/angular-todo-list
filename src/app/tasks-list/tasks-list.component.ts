4d21a2e88abcb1d02890977af13f23a40b9174eb
NERFTHIS <mustaevs@gmail.com>
2024-03-20 14:13:54 +0300

    Use ngFor to generate task items

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TaskComponent } from '../task/task.component';
+import { TasksService } from '../services/tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, CommonModule],
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.scss',
 })
-export class TasksListComponent {}
+export class TasksListComponent {
+  tasksService = inject(TasksService);
+  tasks = this.tasksService.tasks;
+}
