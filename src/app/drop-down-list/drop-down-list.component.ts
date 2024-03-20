c7ba2eb9b05bac5073f52dac7769c06593d95e71
NERFTHIS <mustaevs@gmail.com>
2024-03-20 20:41:19 +0300

    Change the dropdown text depending on task status

--- src/app/drop-down-list/drop-down-list.component.ts
+++ src/app/drop-down-list/drop-down-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-drop-down-list',
   standalone: true,
   imports: [],
   templateUrl: './drop-down-list.component.html',
-  styleUrl: './drop-down-list.component.scss'
+  styleUrl: './drop-down-list.component.scss',
 })
-export class DropDownListComponent {
+export class DropDownListComponent implements OnInit {
+  @Input() isTaskImportant!: boolean;
+  dropDownText!: string;
 
+  ngOnInit(): void {
+    this.dropDownText = this.isTaskImportant ? 'regular' : 'important';
+  }
 }
