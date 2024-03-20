547fc0dc5d3285f699d93a7a901333419dce84b8
NERFTHIS <mustaevs@gmail.com>
2024-03-20 13:15:29 +0300

    Add layout in task-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TaskFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo-list';
