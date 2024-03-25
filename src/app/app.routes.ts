1e70a232900cc03876c34400d215aa4189edb758
NERFTHIS <mustaevs@gmail.com>
2024-03-25 19:27:51 +0300

    Add auth page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './todo/pages/todo/todo.component';
+import { AuthComponent } from './auth/pages/auth/auth.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    title: 'auth',
+    component: AuthComponent,
+  },
+
   {
     path: 'todo',
     title: 'todo',
