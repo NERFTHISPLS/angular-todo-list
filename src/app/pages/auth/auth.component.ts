daf612221af64d7f55bc3b1643282615643fd25e
NERFTHIS <mustaevs@gmail.com>
2024-03-26 01:19:01 +0300

    Get inputs value from form

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  public authForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  public submitAuth() {
+    console.log(this.authForm.value.email);
+    console.log(this.authForm.value.password);
+  }
+}
