20abcbad4da8996c8f0374d9cea7aa198850d7c4
NERFTHIS <mustaevs@gmail.com>
2024-03-25 19:23:43 +0300

    Display todo page on page root and fix a bug with container class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TodoComponent } from './todo/pages/todo/todo.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './todo/pages/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'todo',
+    title: 'todo',
+    component: TodoComponent,
+  },
+];
