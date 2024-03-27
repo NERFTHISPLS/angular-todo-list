9f074e059cea0816115773f77b3f93560ed28550
NERFTHIS <mustaevs@gmail.com>
2024-03-27 23:24:33 +0300

    Create template and styles for navbar

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -3,11 +3,17 @@ import { TasksSearchComponent } from '../../shared/components/todo/tasks-search/
 import { TasksListComponent } from '../../shared/components/todo/tasks-list/tasks-list.component';
 import { TaskFormComponent } from '../../shared/components/todo/task-form/task-form.component';
 import { TaskEventValue } from '../../interfaces/task';
+import { NavbarComponent } from '../../navbar/navbar.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [TaskFormComponent, TasksListComponent, TasksSearchComponent],
+  imports: [
+    TaskFormComponent,
+    TasksListComponent,
+    TasksSearchComponent,
+    NavbarComponent,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
