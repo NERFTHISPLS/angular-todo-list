9f3e3b5459acdb4f6cdd7be8337cb2c8901e8923
NERFTHIS <mustaevs@gmail.com>
2024-03-27 18:50:08 +0300

    Add form validation

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../shared/services/auth.service';
 import { User } from '../../interfaces/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
@@ -16,12 +17,21 @@ export class AuthComponent {
     password: new FormControl(''),
   });
 
+  public submitted = false;
+
   constructor(private _authService: AuthService) {}
 
   public submitAuth() {
+    this.submitted = true;
+
+    const { email, password } = this.authForm.value;
+
+    if (email === '' || password === '') return;
+
     this._authService
       .login('jim@dundermifflin.com', 'password') // temp data
       .subscribe((user: User | null) => {
+        this.submitted = false; // reverting to initial state
         console.log(user);
       });
   }
